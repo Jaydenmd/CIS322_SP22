403cc307f82947e06c104e6f813c76e543652694
Jaydenmd <85521823+Jaydenmd@users.noreply.github.com>
2022-02-08 22:47:19 -0800

    Update test_cards.py
    Overall, I don't believe that there are much changes needed, it seems pretty efficient! However, some ways you might be able to make it a little more efficient is by making the ordered deck and shuffled deck all be on the same line so it says ordered_deck, shuffled_deck = Deck() it might be useful.
    
    Also the else that makes it reshuffle when a match is not made might also be useful, and worth checking out if you want. But overall, great job bro!

--- test/test_cards.py
+++ test/test_cards.py
@@ -1,7 +1,7 @@
 from testing_base import *
 
 def compare_shuffled_deck():
-    ordered_deck = Deck()
+    ordered_deck = Deck()                                                       #This seems pretty nice! I guess if you want for more efficiency, you could assign both of the variabes to Deck() on one line
     shuffled_deck = Deck()
     shuffled_deck.shuffle()
 
@@ -9,7 +9,7 @@ def compare_shuffled_deck():
 
     num_matches = 0
     for idx, card in enumerate(ordered_deck.cards):
-        if card == shuffled_deck.cards[idx]: num_matches += 1
+        if card == shuffled_deck.cards[idx]: num_matches += 1                   #It might also be beneficial to try an else that makes it reshuffle if you want to
 
     return num_matches
 
