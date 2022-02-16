80ae10bde60b9227d8cab8806c77c6702ec419b1
Jaydenmd <85521823+Jaydenmd@users.noreply.github.com>
2022-02-15 21:49:09 -0800

    Update test_cards.py
    It seems like you are getting creative when it comes to making pairs for the cards

--- test/test_cards.py
+++ test/test_cards.py
@@ -1,6 +1,6 @@
 from testing_base import *
 
-def compare_shuffled_deck():
+def compare_shuffled_deck():                                                    
     ordered_deck = Deck()
     shuffled_deck = Deck()
     shuffled_deck.shuffle()
@@ -9,7 +9,7 @@ def compare_shuffled_deck():
 
     num_matches = 0
     for idx, card in enumerate(ordered_deck.cards):
-        if card == shuffled_deck.cards[idx]: num_matches += 1
+        if card == shuffled_deck.cards[idx]: num_matches += 1           #It looks like you have a good approach for your code bro!
 
     return num_matches
 
